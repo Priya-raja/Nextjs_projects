c3c42b03c0b3c2df54e8990c8e5f8f8e20998f6f
Priya-raja <priya.thevar89@gmail.com>
2021-05-04 22:52:29 +0530

    added enpoint api

--- content-manager-app-api/index.js
+++ content-manager-app-api/index.js
@@ -4,18 +4,46 @@ const PORT = 3001;
 
 const fs = require("fs");
 const path = require("path");
+
+// const cors = require("cors")
+
 const pathToFile = path.resolve("./data.json");
 console.log(pathToFile);
 
+
+// var corsOptions = {
+//     origin: 'http://localhost/3000',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+//   app.use(cors(corsOptions))
+
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
+app.use(express.json());
 app.get("/",(req, res) => {  
     res.send("Hello World")
 })
 
-app.get("/api/resources",(req, res) => {
-    
+app.get("/api/resources",(req, res) => { 
     const resources = getResources();
-    res.send(resources);
+    res.send(resources);  
+})
+
+app.post("/api/resources",(req, res) => { 
+    const resources = getResources();
+   
+    const resource = req.body;
+    resource.createdAt = new Date();
+    resource.status = "inactive";
+    resource.id = Date.now().toString();
+    resources.unshift(resource);
+    fs.writeFile(pathToFile,JSON.stringify(resources, null, 2), (error) =>{
+        if(error) {
+           return res.status(422).send("Data are missing!");
+        }
+        return res.send("Data has been saved");
+    }
+    )
+    
     
 })
 
